92cb56ab809f18d3271c1995410167348e7015fd
Yehuda Katz <wycats@gmail.com>
2012-10-27 17:14:47 -0700

    Add support for responding to a query

--- lib/tokaido/dns/query.rb
+++ lib/tokaido/dns/query.rb
@@ -14,6 +14,22 @@ module Tokaido
       def name
         @raw_question[0].to_s
       end
+
+      def response_name
+        @raw_question[0]
+      end
+
+      def response_class
+        @raw_question[1]
+      end
+    end
+
+    class Answer
+      attr_writer :data, :ttl
+
+      def add_to_response(question, response)
+        response.add_answer question.response_name, @ttl, question.response_class.new(*@data)
+      end
     end
 
     class Query
@@ -28,6 +44,23 @@ module Tokaido
 
       def questions
         @questions ||= @message.question.map { |m| Question.new(m) }
+      end
+
+      def respond
+        response = Resolv::DNS::Message.new @message.id
+        response.qr = 1 # response
+        response.opcode = @message.opcode # same as request
+        response.aa = 1 # authoritative
+        response.rd = @message.rd # recursion desired same as request
+        response.ra = 0 # does not support recursion
+        response.rcode = 0 # no error (TODO: Error handling)
+
+        questions.each do |question|
+          answer = Answer.new.tap { |a| yield question, a }
+          answer.add_to_response(question, response)
+        end
+
+        response
       end
     end
   end
--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -1,5 +1,11 @@
 require "resolv"
 
+# See http://www.networksorcery.com/enp/protocol/dns.htm for a full
+# description of the underlying protocol
+
+# See https://github.com/ruby/ruby/blob/trunk/lib/resolv.rb for the
+# Ruby Message implementation that we are using in these tests
+
 describe "Tokaido::DNS::Query.decode" do
   before do
     m = Resolv::DNS::Message.new    
@@ -23,5 +29,22 @@ describe "Tokaido::DNS::Query.decode" do
   it "knows the domain of each question" do
     question = @query.questions.first
     question.name.should == "foo.tok"
+  end
+
+  it "knows how to prepare a response to a query" do
+    message = @query.respond do |question, response|
+      question.type.should == "A"
+      question.name.should == "foo.tok"
+
+      response.data = "127.0.0.1"
+      response.ttl = 60
+    end
+
+    message.qr.should == 1 # response
+    message.opcode.should == 0 # same as request
+    message.aa.should == 1 # authoritative
+    message.rd.should == 1 # same as request
+    message.ra.should == 0 # recursion not available
+    message.rcode.should == 0 # no error
   end
 end
